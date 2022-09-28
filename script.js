e60502dd951170f4e9d0e36a0d093ad7d68988ec
adamk9516 <114585789+adamk9516@users.noreply.github.com>
2022-09-28 08:38:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Adam K"; // update the part BETWEEN the "quotes"
+let userName = "adamk9516"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
